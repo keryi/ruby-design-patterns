11aa121965f59189774e22971f89c693c546ebf1
keryi <hti_32@yahoo.com>
2015-08-15 06:19:27 +0000

    strategy pattern

---
+++ strategy/formatter.rb
@@ -0,0 +1,5 @@
+class Formatter
+  def output_report(title, text)
+    raise 'Abstract method called: output_report'
+  end
+end
---
+++ strategy/main.rb
@@ -0,0 +1,12 @@
+require_relative 'report'
+require_relative 'html_formatter'
+require_relative 'plain_text_formatter'
+
+report = Report.new(HtmlFormatter.new)
+puts '((((( HTML REPORT )))))'
+report.output_report
+
+puts
+puts '((((( PLAIN TEXT REPORT )))))'
+report.formatter = PlainTextFormatter.new
+report.output_report
