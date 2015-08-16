b21619646be5d5992cf8304a8764258c8273ac84
keryi <hti_32@yahoo.com>
2015-08-16 05:31:20 +0000

    notify only when a consistent set of changes is complete

--- observer/main.rb
+++ observer/main.rb
@@ -1,12 +1,15 @@
 require_relative 'employee'
 require_relative 'payroll'
 require_relative 'taxman'
+require_relative 'promoter'
 
 e = Employee.new('John', 'Software Engineer', 100_000)
 e.add_observer(Payroll.new)
 e.add_observer(TaxMan.new)
+e.add_observer(Promoter.new)
 
-puts 'Raise time...'
+puts 'Raise & Promote time...'
 
 e.salary = 150_000
-
+e.title = 'Chief Engineer'
+e.changes_complete
---
+++ observer/promoter.rb
@@ -0,0 +1,5 @@
+class Promoter
+  def update(employee)
+    puts "#{employee.name} just got promoted to #{employee.title}"
+  end
+end
