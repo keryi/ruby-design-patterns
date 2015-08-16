6ad05a8c32f515e16d0620e7c69857316f0fb16b
keryi <hti_32@yahoo.com>
2015-08-16 06:04:37 +0000

    refactor - extract composite task to a base class

--- composite/make_batter_task.rb
+++ composite/make_batter_task.rb
@@ -1,24 +1,12 @@
-require_relative 'task'
+require_relative 'composite_task'
 require_relative 'add_dry_ingredient_task'
 require_relative 'mix_task'
 
-class MakeBatterTask < Task
+class MakeBatterTask < CompositeTask
   def initialize
     super 'Make Batter'
     @sub_tasks = []
     add_sub_task AddDryIngredientTask.new
     add_sub_task MixTask.new
-  end
-
-  def add_sub_task(task)
-    @sub_tasks << task
-  end
-
-  def remove_sub_task(task)
-    @sub_tasks.delete task
-  end
-
-  def get_time_required
-    @sub_tasks.map(&:get_time_required).inject(:+)
   end
 end
